043681d353d7382db0c21ca8478ae311b5dc3a27
Kage Park <kagepark1@gmail.com>
2018-01-23 10:36:28 -0800

    add uninstall xcat script

---
+++ sbin/uninstall_xcat.sh
@@ -0,0 +1,29 @@
+error_exit() {
+   echo $*
+   exit 1
+}
+[ "$(id -u)" == "0" ] || error_exit "it need root account"
+echo -n "Are you sure remove xCAT and KxCAT (y/[n])?"
+read dx
+[ "$dx" == "y" ] || error_exit "stopped uninstall"
+
+dumpxCATdb -p xcat.db
+lsdef -z all
+lsdef -z -t network -l
+nodeset all offline
+for ii in $(kxcat groups | awk '{print $1}'); do
+   makedhcp -d $ii
+done
+makedns -n
+systemctl stop xcatd 
+rpm -e $(echo $(rpm -qa |grep -i xcat))
+rm -fr ~/.xcat
+rm -fr /install
+rm -fr /tftpboot
+rm -fr /etc/xcat
+rm -fr /etc/sysconfig/xcat
+rm -f /etc/profile.d/xcat.*
+rm -fr /tmp/genimage*
+rm -fr /tmp/packimage*
+rm -fr /tmp/mknb*
+
