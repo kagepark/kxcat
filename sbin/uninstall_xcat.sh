61af1203ff03bd8465456309b78fe5fa1a4d16f2
Kage Park <kagepark1@gmail.com>
2018-01-24 15:43:08 -0800

    Add comment

--- sbin/uninstall_xcat.sh
+++ sbin/uninstall_xcat.sh
@@ -7,21 +7,27 @@ echo -n "Are you sure remove xCAT and KxCAT (y/[n])?"
 read dx
 [ "$dx" == "y" ] || error_exit "stopped uninstall"
 
+echo "Backup DB"
 dumpxCATdb -p xcat.db
 lsdef -z all > xcat.db.log
 lsdef -z -t network -l >> xcat.db.log
+echo "Power off whole nodes"
 nodeset all offline 
+echo "Remove nodes"
 . /etc/profile.d/kxcat.sh
 for ii in $(kxcat groups | awk '{print $1}'); do
    makedhcp -d $ii
    makehosts -d $ii
 done
 makedns -n
+echo "Stop daemons"
 systemctl stop xcatd 
 systemctl stop dhcpd
 systemctl stop nfs
 systemctl stop httpd
+echo "Uninstall xCAT"
 rpm -e $(echo $(rpm -qa |grep -i xcat))
+echo "clean up"
 rm -fr ~/.xcat
 rm -fr /install
 rm -fr /tftpboot
