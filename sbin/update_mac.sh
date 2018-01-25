50649ab58b50d0a9022a315eee2fdf70b685c53d
Kage Park <kagepark1@gmail.com>
2018-01-24 18:11:06 -0800

    update code for node information update (BMC IP/MAC)

--- sbin/update_mac.sh
+++ sbin/update_mac.sh
@@ -1,9 +1,20 @@
 if [ -f server_list.cfg ]; then
+   . $(dirname $(dirname $(readlink -f $0)))/etc/xcat.sh
+   node_list=()
+   while read x ; do
+       id=$(echo $x | awk -F, '{print $2}');
+       [ -n "$id" ] && node_list[$id]=$(echo $x | awk -F, '{print $1}');
+   done < <(echo $(lsdef -t node -o all -i id) | sed "s/id=/,/g" | sed "s/Object name: /\n/g" | sed "s/ ,/,/g")
+   
    for ii in $(cat server_list.cfg); do
-      srv_id=$(echo $ii | awk -F\| '{print $1}')
-      srv_info=$(echo $ii | awk -F\| '{print $2}')
-      srv_name=node-$(printf "%05d" $srv_id)
-      chdef -t node $srv_name mac=$srv_mac
+      node_id=$(echo $ii | awk -F\| '{print $1}')
+      node_name=${node_list[$node_id]}
+      node_update=""
+      node_mac=$(echo $ii | awk -F\| '{print $2}')
+      [ -n "$node_mac" ] && node_update="mac=$node_mac"
+      node_bmc_ip=$(echo $ii | awk -F\| '{print $3}')
+      [ -n "$node_bmc_ip" ] && node_update="$node_update bmc=$node_bmc_ip"
+      [ -n "$node_update" ] && chdef -t node $node_name $node_update
    done
 else
    echo "server_list.cfg file not found"
