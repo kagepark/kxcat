fd3b7449ad54c7aa6ce3f25843eb638c617a5d73
kage Park <kagepark1@gmail.com>
2018-02-14 18:51:02 -0500

    Add iptable example script. Fix a bug for kgt tool install url

---
+++ sbin/iptables.example.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+outside_net_dev=$1
+shift 1
+if [ ! -n "$outside_net_dev" -o ! -d /sys/class/net/$outside_net_dev ]; then
+     echo "$(basename $0) <outside network dev> [<adding ip1> ...]"
+     exit
+fi
+
+outside_ip=$(ifconfig $outside_net_dev | grep " inet " | awk '{print $2}')
+
+iptables -t nat -A POSTROUTING -s ${outside_ip}/32 -o ${outside_net_dev}  -j MASQUERADE
+for add_ip in $*; do
+   iptables -t nat -A POSTROUTING -s ${add_ip}/32 -o ${outside_net_dev}  -j MASQUERADE
+done
+iptables -A INPUT -i ${outside_net_dev} -p TCP --dport ssh -j ACCEPT
+iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+iptables -A INPUT -m state --state NEW -i !${outside_net_dev} -j ACCEPT
+iptables -A INPUT -i ${outside_net_dev} -j DROP 
