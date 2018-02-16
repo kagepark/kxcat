4435d6b5384e9aed8be0dd192ec378a0e33e596e
kage Park <kagepark1@gmail.com>
2018-02-16 14:29:13 -0500

    Update upgrade script force mode

--- sbin/upgrade_kxcat.sh
+++ sbin/upgrade_kxcat.sh
@@ -13,6 +13,15 @@ error_exit() {
    exit 1
 }
 
+if [ "$1" == "force" ]; then
+   _KXCAT_HOME=$(dirname $(readlink -f $0))
+   git reset --hard HEAD
+   git clean -f -d
+   git fetch -all
+   sed -i "/^_KXC_VERSION=/c \
+_KXC_VERSION=$(git describe --tags) " $_KXCAT_HOME/../bin/kxcat
+   exit
+fi
 if [ -f /etc/profile.d/kxcat.sh ]; then
    . /etc/profile.d/kxcat.sh
    cd $_KXCAT_HOME
