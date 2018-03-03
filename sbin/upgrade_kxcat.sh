55c67083fc7ab86c7a330288eef0df3246a7b56a
kage Park <kagepark1@gmail.com>
2018-03-02 16:05:03 -0800

    remove garbage file and remove some comments

--- sbin/upgrade_kxcat.sh
+++ sbin/upgrade_kxcat.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 ###########################################################
-# Kage Park
-# xCAT Installer
-# Re-designed at 10/06/2017 by Kage Park
-# Base design Using old libraries(2006/07) of Kage
 # License : GPL
 ###########################################################
 #set +x
