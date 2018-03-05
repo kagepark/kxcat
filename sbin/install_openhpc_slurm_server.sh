d76d93da9a2394e99e39c0bcf28b7d00fbf758a2
kage Park <kagepark1@gmail.com>
2018-03-05 12:01:06 -0800

    Update repo for openhpc

--- sbin/install_openhpc_slurm_server.sh
+++ sbin/install_openhpc_slurm_server.sh
@@ -1,2 +1,11 @@
-yum -c aaa install ohpc-slurm-server
+echo "[OpenHPC]
+name=OpenHPC Pakages
+baseurl=/install/post/otherpkgs/centos7.4/OpenHPC
+enabled=1
+gpgcheck=0" > /tmp/openhpc.repo
+
+
+yum -c /tmp/openhpc.repo install ohpc-slurm-server
 perl -pi -e "s/ControlMachin=\S+/ControlMachine=$(hostname)/" /etc/slurm/slurm.conf
+rm -f /tmp/openhpc.repo
+
