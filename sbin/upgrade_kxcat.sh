e3da30c56533157b8bff0e82e2efc58fa820bf29
Kage Park <kagepark1@gmail.com>
2018-05-01 17:44:16 -0700

    update upgrade script

--- sbin/upgrade_kxcat.sh
+++ sbin/upgrade_kxcat.sh
@@ -9,6 +9,20 @@ error_exit() {
    exit 1
 }
 
+update_scripts() {
+   local home_path
+   home_path=$1
+   for ii in state_update kxcatboot syslog;do
+       [ -f $home_path/share/$ii ] && cp -a $home_path/share/$ii /install/postscripts/$ii
+   done
+   for ii in 0000_update_state 0001_bmc_set  0001_cleanyum  0002_mpi_net  0003_ntp; do
+       [ -f $home_path/share/$ii ] && cp -a $home_path/share/$ii /install/postscripts/xcat_boot.d/$ii
+   done
+   for ii in 0000_home_mount;do
+       [ -f $home_path/share/$ii ] && cp -a $home_path/share/$ii /global/xcat_boot.d/global/$ii
+   done
+}
+
 if [ "$1" == "force" ]; then
    _KXCAT_HOME=$(dirname $(readlink -f $0))
    git reset --hard HEAD
